a5721b89bf80b130678b8bba65b8542b5ee43d6f
Jayonto Dutta Plabon <plabondutta0@gmail.com>
2024-06-23 20:05:50 +0600

    Create 2. Datatypes.java

---
+++ 2. Datatypes.java
@@ -0,0 +1,33 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        // Most Useful
+
+        int num = 123; // Stores whole numbers from -2,147,483,648 to 2,147,483,647
+        System.out.println("Integer: " + num);
+
+        long num2 = 123456789L; // Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        System.out.println("Long: " + num2);
+
+        float num3 = 12.13f; // Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+        System.out.println("Float: " + num3);
+
+        double num4 = 12345.12345534; // Stores fractional numbers. Sufficient for storing 15 decimal digits
+        System.out.println("Double: " + num4);
+
+        char ch = 'a'; // Stores a single character/letter or ASCII values
+        System.out.println("Character: " + ch);
+
+        String s = "Hello";
+        System.out.println("String: " + s);
+
+        boolean flag = true; // Stores true or false values
+        System.out.println("Boolean: " + flag);
+
+        // Some More
+
+        byte num5 = 12; // Stores whole numbers from -128 to 127
+        System.out.println("The Byte Number is: " + num5);
+
+        short num6 = 12323; // Stores whole numbers from -32,768 to 32,767
+    }
+}
