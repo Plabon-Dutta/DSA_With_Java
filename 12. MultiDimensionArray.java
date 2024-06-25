59b7be859280554bcd74ece5a6f20d93c5fe8f96
Jayonto Dutta Plabon <plabondutta0@gmail.com>
2024-06-25 21:27:08 +0600

    Create 12. MultiDimensionArray.java

---
+++ 12. MultiDimensionArray.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MultiDimensionArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = input.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        for (int[] i : arr) {
+            System.out.println(Arrays.toString(i));
+        }
+    }
+}
